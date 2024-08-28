44f1011f860ad05fe1e15eac692f0286be032f8d
Raxxoht <oskar.hauge123@gmail.com>
2024-08-28 04:35:11 +0200

    Added a start screen

--- Moleculez/game.py
+++ Moleculez/game.py
@@ -18,26 +18,60 @@ class Main: # Class definition of the main program.
         game.init()
         self.screen = game.display.set_mode((window_x, window_y))
         game.display.set_caption("Moleculez")
+        self.fontM = game.font.SysFont("Arial", 48)
+        self.fontS = game.font.SysFont("Arial", 32)
 
-        self.running = True
+        self.state = "Menu"
     
     def quit(self): # Quits the game and exits the window
         game.quit()
         sys.exit()
 
-    def run(self): # Running functions that need to be updated per tick
-        while self.running:
+    def mainMenu(self): # Main menu function to display a main menu while the state says so
+        game.display.set_caption("Main Menu")
+        while self.state == "Menu":
+            self.screen.fill("black")
             self.eventHandler()
-        self.quit()
+
+            welcome_text = self.fontM.render("Welcome", True, (50, 205, 50)) # Cool text formatting
+            to_text = self.fontM.render("to", True, (173, 216, 230))
+            moleculez_text = self.fontM.render("Moleculez!", True, (255, 0, 255))
+            press_text = self.fontS.render("Press any key to start", True, (255,255,255))
+
+            welcome_rect = welcome_text.get_rect(center=(window_x // 2 - 120, window_y // 2))
+            to_rect = to_text.get_rect(center=(window_x // 2, window_y // 2))
+            moleculez_rect = moleculez_text.get_rect(center=(window_x // 2 + 120, window_y // 2))
+            press_rect = press_text.get_rect(center=(window_x // 2, window_y//2 + 200))
+
+            self.screen.blit(welcome_text, welcome_rect) # Separate blitting for axis transformation
+            self.screen.blit(to_text, to_rect)
+            self.screen.blit(moleculez_text, moleculez_rect)
+            self.screen.blit(press_text, press_rect)
+
+            self.update()
+
+    def game(self):
+        game.display.set_caption("Game")
+        while self.state == "Game":
+
+            self.screen.fill("black")
+            self.eventHandler()
+            self.update()
+
 
     def eventHandler(self): # Handling of events, mouseclicks, mousepositions and keyclicks
         for e in game.event.get():
             if e.type == game.QUIT:
-                self.running = False
+                self.state = False
+                self.quit()
+            if e.type == game.KEYDOWN:
+                if self.state == "Menu":
+                    self.state = "Game"
+                    self.game()
 
     def update(self): # Main update functions. Updates variables such as: Positions, speeds and object lists
-        pass
+        game.display.flip() 
 
 if __name__ == "__main__":  # Main instantiator
     Main = Main()
-    Main.run()
+    Main.mainMenu()
